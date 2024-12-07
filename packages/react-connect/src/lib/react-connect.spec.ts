933eb7a69602f8b968522ef3c7e6f0bb49a660bb
mathieueveillard <mathieu.eveillard@gmail.com>
2024-12-08 00:47:13 +0100

    Setup Nx monorepo

---
+++ packages/react-connect/src/lib/react-connect.spec.ts
@@ -0,0 +1,7 @@
+import { reactConnect } from './react-connect';
+
+describe('reactConnect', () => {
+  it('should work', () => {
+    expect(reactConnect()).toEqual('react-connect');
+  });
+});
---
+++ packages/react-connect/src/lib/react-connect.ts
@@ -0,0 +1,3 @@
+export function reactConnect(): string {
+  return 'react-connect';
+}
